2a68991dac0ca8ef5f4545f654ca5c687a2a9320
evilboss <jr.evilboss@gmail.com>
2020-03-26 18:40:42 +0800

    add editing contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,36 @@ import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
-import {Accordion, Card, Button, Badge} from "react-bootstrap";
+import {Accordion, Card, Button, Badge, Form} from "react-bootstrap";
 import {deleteContact, updateContact} from '../actions'
 
 const Contact = ({dispatch, onClick, completed, name, address, email, id}) => {
     let [open, setOpen] = useState(false);
+    let [editName, setEditName] = useState(false);
+    let [editEmail, setEditEmail] = useState(false);
+    const updateContact = (event) => {
+        console.log('event fire', event);
+        if (event.key === 'Enter') {
+            console.log('update contact');
+        }
+
+    };
     return (
         <Accordion defaultActiveKey="0">
             <Card>
                 <Card.Header>
-                    {name}
+
+                    {editName ? <input/> : name}
+
+
+                    <span className="action"
+                          onClick={() => {
+                              dispatch(deleteContact(id));
+                              console.log('delete');
+                          }}>
+                        <i className="fas fa-trash" title="Delete Contact"/>
+
+                    </span>
                     <Accordion.Toggle as={Button} variant="link" eventKey="0" onClick={() => {
                         console.log('click');
                         setOpen(!open)
@@ -19,18 +39,18 @@ const Contact = ({dispatch, onClick, completed, name, address, email, id}) => {
                         <Badge variant="secondary"> <i className={`fas fa-chevron-${open ? 'up' : 'down'}`}/></Badge>
 
                     </Accordion.Toggle>
-
-                    <span className="action"
-                          onClick={() => {
-                              dispatch(deleteContact(id));
-                              console.log('delete');
-                          }}>
-                    <i class="fas fa-trash" title="Delete Contact"/>
-                </span>
                 </Card.Header>
                 <Accordion.Collapse eventKey="0">
                     <Card.Body>
-                        <Card.Text>Email : {email}</Card.Text>
+                        <Card.Text>Email : {editEmail ?
+                            <Form.Control type="text" placeholder={email} onKeypress={(e) => {
+                                console.log('keypress fire',e);
+                                updateContact(e);
+                            }}/>
+                            : <span onDoubleClick={() => {
+                                setEditEmail(true);
+                            }}>{email ? email : 'double click to add'}</span>}
+                        </Card.Text>
                         <Card.Text>
                             Address: {address}
                         </Card.Text>
