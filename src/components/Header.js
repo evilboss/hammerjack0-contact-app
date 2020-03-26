218ff16c7aa5759fe44997253c70c52c99543658
evilboss <jr.evilboss@gmail.com>
2020-03-26 16:35:53 +0800

    update link styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import {Form, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
     Link
 } from "react-router-dom";
 
@@ -13,8 +10,8 @@ const Header = () => (
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Link to="/">Home</Link>
-                <Link to="/contacts">Contacts</Link>
+                <Link style={{color: 'white'}} to="/">Home</Link>
+                <Link style={{color: 'white'}}  to="/contacts">Contacts</Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
