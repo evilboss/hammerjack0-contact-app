76cb9b11d1ca5e6adad19b7532025840a1560337
evilboss <jr.evilboss@gmail.com>
2020-03-26 18:14:06 +0800

    add custom open state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,47 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
-import {Accordion, Card, Button} from "react-bootstrap";
+import {Accordion, Card, Button, Badge} from "react-bootstrap";
 import {deleteContact, updateContact} from '../actions'
 
-const Contact = ({dispatch, onClick, completed, name, address, email, id}) => (
-    <Accordion defaultActiveKey="0">
-        <Card>
-            <Card.Header>
-                {name}
-                <span className="action"
-                onClick={() => {
-                    dispatch(deleteContact(id));
-                    console.log('delete');
-                }}>
-                    <i class="fas fa-trash" title="Delete Contact" />
+const Contact = ({dispatch, onClick, completed, name, address, email, id}) => {
+    let [open, setOpen] = useState(false);
+    return (
+        <Accordion defaultActiveKey="0">
+            <Card>
+                <Card.Header>
+                    {name}
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0" onClick={() => {
+                        console.log('click');
+                        setOpen(!open)
+                    }}>
+                        <Badge variant="secondary"> <i className={`fas fa-chevron-${open ? 'up' : 'down'}`}/></Badge>
+
+                    </Accordion.Toggle>
+
+                    <span className="action"
+                          onClick={() => {
+                              dispatch(deleteContact(id));
+                              console.log('delete');
+                          }}>
+                    <i class="fas fa-trash" title="Delete Contact"/>
                 </span>
-            </Card.Header>
-            <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                    <Card.Text>Email : {email}</Card.Text>
-                    <Card.Text>
-                        Address: {address}
-                    </Card.Text>
-                    <Button onClick={() => {
-                        dispatch(updateContact({
-                            id,
-                            name: 'test name',
-                            address: 'test address',
-                            email: 'test email'
-                        }));
-                        console.log('update');
-                    }}>Update contact</Button>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <Card.Text>Email : {email}</Card.Text>
+                        <Card.Text>
+                            Address: {address}
+                        </Card.Text>
+                        <Button onClick={() => {
+                            dispatch(updateContact({
+                                id,
+                                name: 'test name',
+                                address: 'test address',
+                                email: 'test email'
+                            }));
+                            console.log('update');
+                        }}>Update contact</Button>
 
-                </Card.Body>
+                    </Card.Body>
 
 
-            </Accordion.Collapse>
-        </Card>
-    </Accordion>
-
-);
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+    )
+};
 
 Contact.propTypes = {
     onClick: PropTypes.func.isRequired,
